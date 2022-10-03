41abc4c50392dbf7bd3c2b273c072094499f764c
sl0wlydeadly <thor93andrew@gmail.com>
2022-10-03 19:27:30 +0200

    added tests

--- src/test/kotlin/MarsRoverTest.kt
+++ src/test/kotlin/MarsRoverTest.kt
@@ -6,11 +6,11 @@ internal class MarsRoverTest {
 
     @Test
     fun mars_rover_receives_commands_and_results_to_the_wanted_position() {
-        val coordinate1 = Coordinates(1, 2)
+        val coordinates = Coordinates(1, 2)
         val command: List<Command> = listOf(Command.L, Command.F, Command.L, Command.F,
             Command.L, Command.F, Command.L, Command.F, Command.F)
 
-        val marsRover = MarsRover(coordinate1, Direction.N)
+        val marsRover = MarsRover(coordinates, Direction.N)
         val plateau = Plateau(Coordinates(5, 5), listOf(marsRover))
         marsRover.executeCommand(command, plateau)
         println("$marsRover")
@@ -22,11 +22,11 @@ internal class MarsRoverTest {
 
     @Test
     fun mars_rover_receives_other_commands_and_results_to_wanted_position() {
-        val coordinate = Coordinates(3, 3)
+        val coordinates = Coordinates(3, 3)
         val command: List<Command> = listOf(Command.F, Command.F, Command.R, Command.F,
             Command.F, Command.R, Command.F, Command.R, Command.R, Command.F)
 
-        val marsRover = MarsRover(coordinate, Direction.E)
+        val marsRover = MarsRover(coordinates, Direction.E)
         val plateau = Plateau(Coordinates(5, 5), listOf(marsRover))
         marsRover.executeCommand(command, plateau)
         println("$marsRover")
@@ -34,5 +34,32 @@ internal class MarsRoverTest {
         assertEquals(5, marsRover.coordinates.x)
         assertEquals(1, marsRover.coordinates.y)
         assertEquals(Direction.E, marsRover.facing)
+    }
+
+    @Test
+    fun two_mars_rovers_receive_their_commands() {
+        val coordinates1 = Coordinates(1, 2)
+        val coordinates2 = Coordinates(3, 3)
+        val command1: List<Command> = listOf(Command.L, Command.F, Command.L, Command.F,
+            Command.L, Command.F, Command.L, Command.F, Command.F)
+        val command2: List<Command> = listOf(Command.F, Command.F, Command.R, Command.F,
+            Command.F, Command.R, Command.F, Command.R, Command.R, Command.F)
+
+        val marsRover1 = MarsRover(coordinates1, Direction.N)
+        val marsRover2 = MarsRover(coordinates2, Direction.E)
+        val plateau = Plateau(Coordinates(5, 5), listOf(marsRover1))
+        marsRover1.executeCommand(command1, plateau)
+        println("$marsRover1")
+
+        assertEquals(1, marsRover1.coordinates.x)
+        assertEquals(3, marsRover1.coordinates.y)
+        assertEquals(Direction.N, marsRover1.facing)
+
+        marsRover2.executeCommand(command2, plateau)
+        println("$marsRover2")
+
+        assertEquals(5, marsRover2.coordinates.x)
+        assertEquals(1, marsRover2.coordinates.y)
+        assertEquals(Direction.E, marsRover2.facing)
     }
 }
